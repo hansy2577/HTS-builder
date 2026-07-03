4f90f0b6d6d06d7bded03caae0234326c8f4fbae
hansy2577B <hansyngampaka@gmail.com>
2026-07-03 12:30:51 +0200

    Add files via upload

---
+++ initiating.js
@@ -0,0 +1,50 @@
+/* make GUI */
+
+// GUI
+
+HPS_makeElement("",-5,87,1,(window.innerWidth + 3),3).style.cssText += "background-color: black; opacity: 0.5;"
+HPS_makeElement("",35,13,1,2,70).style.cssText += "background-color: black; opacity: 0.3;"
+HPS_makeElement("",220,13,1,2,70).style.cssText += "background-color: black; opacity: 0.3;"
+HPS_makeElement("",440,13,1,2,70).style.cssText += "background-color: black; opacity: 0.3;"
+
+HPS_makeElement("",-5,165,1,150,3,"tabMeta").style.cssText += "background-color: black; opacity: 0.3;"
+
+HPS_makeElement("element0",-1,550,1,(window.innerWidth - 5),30).style.cssText += "background-color: #4E526E;"
+
+
+// button
+function GUIresize(){
+  HPS_get("top bars",true).width = (window.innerWidth + 0.5) + "px";
+
+  if ((window.innerHeight - 50) <= 550) {
+    // manual
+    HPS_get("element0").remove()
+    HPS_get("move").style.top = (550)+"px";
+    HPS_get("tabMeta").style.top = 0+"px";
+  
+    HPS_makeElement("element0",-1,550,1,(window.innerWidth - 5),30).style.cssText += "background-color: #4E526E;"
+  } else {
+    // auto
+    HPS_get("element0").remove()
+    HPS_get("move").style.top = (window.innerHeight - 25)+"px";
+    HPS_get("tabMeta").style.top = ((window.innerHeight * 0.3) - 140)+"px";
+  
+    HPS_makeElement("element0",-1,(window.innerHeight - 30.8),1,(window.innerWidth + 0.5),30).style.cssText += "background-color: #4E526E;"
+  }
+}
+
+GUIresize()
+window.onresize = function () {
+  GUIresize()
+}
+
+
+HPS_makeImage("save","images/save.png",-50,-85,1,0.28);
+HPS_makeImage("play/stop","images/play.png",10,-85,1,0.2);
+HPS_makeImage("save","images/setting.png",60,-85,1,0.2);
+
+// text
+HPS_makeText("meta text","name: ",-10,146,1,0.55,"tabMeta").style.color = "white"; 
+
+HPS_makeImage("save","images/save.png",-50,-85,1,0.28);
+HPS_makeImage("save","images/save.png",-50,-85,1,0.28);
---
+++ main.js
@@ -0,0 +1,170 @@
+let engine = {
+  version: "1.0",
+  elementMakeCount: 0,
+  oldSelect: null,
+  select: null,
+  currentId: null,
+  projectName: "untitled",
+  dataSave: null,
+  element: {}
+}
+
+document.getElementById("import images").addEventListener('change', function(event) {
+  const file = event.target.files[0];
+            
+  if (file && file.type.startsWith('image/')) {
+    const reader = new FileReader();
+                
+    reader.onload = function(e) {
+      var euh = document.createElement("img");
+      document.getElementById("canvas").appendChild(euh);
+      
+      euh.id = file.name;
+      euh.style.position = 'absolute';
+      euh.style.top = '100px'
+      euh.style.left = '100px'
+      euh.src = e.target.result;
+      euh.name = (engine.elementMakeCount+":P"); 
+      euh.onclick = function (e) {
+        select(euh.id)
+      }
+      engine.elementMakeCount++;
+      
+      alert("'"+file.name + "' add successful !")
+    };
+                
+    reader.readAsDataURL(file);
+  }
+});
+
+document.getElementById("import project").addEventListener('change', function(event) {
+  
+  const file = event.target.files[0];
+  const reader = new FileReader();
+                
+  reader.onload = function(e) {
+    var dataFile = JSON.parse(e.target.result);
+    console.log(dataFile);
+    engine.elementMakeCount = 0;
+    engine.projectName = file.name;
+    
+    if (document.getElementById('canvas').lastElementChild) {
+      document.getElementById('canvas').lastElementChild.remove()
+    }
+    
+    for (var i = 0; i < dataFile.projectData.length; i++) {
+      const euh = document.createElement("img");
+      document.getElementById("canvas").appendChild(euh);
+      
+      euh.id = dataFile.projectData[i].name;
+      euh.style.position = 'absolute';
+      euh.style.left = dataFile.projectData[i].position[0]+'px'
+      euh.style.top = dataFile.projectData[i].position[1]+'px'
+      euh.style.zIndex = dataFile.projectData[i].position[2];
+      euh.style.scale = dataFile.projectData[i].scale;
+      euh.src = dataFile.projectData[i].src;
+      euh.name = (engine.elementMakeCount+":P"); 
+      euh.onclick = function (e) {
+        select(euh.id)
+      }
+      
+      engine.elementMakeCount++;
+      
+      //alert("'"+file.name + "' add successful !")
+    };
+  }
+  reader.readAsText(file);
+
+});
+
+
+function loadProject() {
+  
+}
+
+setInterval(() => {
+  if (engine.select !== null) {
+    document.getElementById(engine.select).style.border = "5px solid blue"
+  } else {
+    if (engine.oldSelect !== null) {
+      document.getElementById(engine.oldSelect).style.border = "0px solid blue"
+    }
+  }
+  
+  if (engine.select !== null) {
+    if (HPS_get("§inputName").value !== "") {
+      HPS_get(engine.select).id = HPS_get("§inputName").value;
+      engine.select = HPS_get("§inputName").value;
+    } else {
+      HPS_get("§inputName").value = " "
+    }
+    
+    HPS_get(engine.select,true).left = HPS_get("§x").value + "px";
+    HPS_get(engine.select,true).top = HPS_get("§y").value + "px";
+    HPS_get(engine.select,true).zIndex = HPS_get("§z").value;
+    HPS_get(engine.select,true).scale = HPS_get("§scale").value;
+  }
+  
+  if (document.title !== "HTS Builder v"+engine.version+" | "+engine.projectName) {
+    document.title = "HTS Builder v"+engine.version+" | "+engine.projectName
+  }
+},0)
+
+function select(name) {
+
+  if (name == engine.select) {
+    HPS_get("§x").value = "";
+    HPS_get("§y").value = "";
+    HPS_get("§z").value = "";
+    HPS_get("§scale").value = "";
+
+    HPS_get("§inputName").value = "";
+    
+    engine.currentId = "";
+    engine.oldSelect = engine.select;
+    engine.select = null;
+  } else {
+    //parseInt('245px',10) for remove px
+    HPS_get("§x").value = parseInt(HPS_get(name,true).left,10);
+    HPS_get("§y").value = parseInt(HPS_get(name,true).top,10);
+    HPS_get("§z").value = HPS_get(name,true).zIndex;
+    HPS_get("§scale").value = HPS_get(name,true).scale;
+
+    HPS_get("§inputName").value = HPS_get(name).id;
+    
+    engine.currentId = HPS_get(name).id;
+    if (engine.select !== null) {
+      engine.oldSelect = engine.select;
+    }
+    engine.select = name;
+    
+    if (engine.oldSelect !== null) {
+      document.getElementById(engine.select).style.border = "0px solid blue";
+      document.getElementById(engine.oldSelect).style.border = "0px solid blue";
+    }
+  }
+}
+
+function saveData() {
+  var loop = 0;
+  var child = null;
+  const myElement = document.getElementById("canvas");
+  engine.dataSave = `{ "project name":"`+engine.projectName+`", "projectData":[`
+  
+  for (var i = 0; i < engine.elementMakeCount; i++) {
+    child = document.getElementsByName(i+":P")[0];
+    //console.log(child)
+    
+    if (i !== engine.elementMakeCount) {
+      engine.dataSave += `{ "name":"`+child.id+`", "position":["`+parseInt(child.style.left)+`","`+parseInt(child.style.top)+`","`+child.style.zIndex+`"], "scale":"`+child.style.scale+`", "src":"`+child.src+`" }`;
+    } 
+    if (i == engine.elementMakeCount - 1) {
+      engine.dataSave += `]}`
+      HPS_saveFile(engine.projectName+".HTP",engine.dataSave);
+      
+      console.log(JSON.parse(engine.dataSave));
+    } else {
+      engine.dataSave += ",                             ";
+    }
+  }
+}
